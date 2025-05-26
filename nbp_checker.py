4463cf5f1c17d08e8707680536e5a99bc340264d
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-05-26 14:45:38 +0200

    Add function to format date for API request

--- nbp_checker.py
+++ nbp_checker.py
@@ -4,6 +4,14 @@ from dateutil import parser
 
 URL = 'https://api.nbp.pl/api/exchangerates/rates/{table}/{code}/{date}/?format=json'
 
+def format_date(date_str: str) -> str:
+    try:
+        parsed_date = parser.parse(date_str)
+        return parsed_date.strftime("%Y-%m-%d")
+    except Exception:
+        raise ValueError("Wrong date format.")
+    
+
 
 
 
