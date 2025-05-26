beec203c9d4074c9ce3e09ba95e72d8b88e7e719
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-05-26 21:43:07 +0200

    Add user input into get_currency() and get_date() functions

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ from nbp_checker import format_date, fetch_exchange_rate
 
 import sys
 
+
+def get_currency() -> str:
+    try:
+        return sys.argv[1]
+    except IndexError:
+        return input("Enter the currency code (e.g. USD, EUR): ").strip()
+
+
+def get_date() -> str:
+    try:
+        return sys.argv[2]
+    except IndexError:
+        return input("Enter the date (e.g. 2025-05-26): ").strip()
+    
+
+currency = get_currency()
+user_input_date = get_date()
